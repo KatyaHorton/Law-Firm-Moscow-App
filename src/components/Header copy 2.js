b1157b6e620fee629ac17aad67c9bf3c68ef9a62
Katya Horton <ekaterina.horton@gmail.com>
2018-08-29 16:14:08 +0100

    Header and MainSlide components added style for phone screen in progress

---
+++ src/components/Header copy 2.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          <h1 className="App-title">«ЮРИДИЧЕСКАЯ КОЛЛЕГИЯ»</h1>
+		  <h2>Решение дел любой сложности</h2>
+        </header>
+
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/MainSlide.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MainSlide extends Component {
+  render() {
+    return (
+        <div id="main-slide">
+          <div id="main-slide-title">
+			  <h1>ЗАКОН И ПРАВО</h1>
+			  <h2>Решение дел любой сложности</h2>
+			</div>
+        </div>
+
+    );
+  }
+}
+
+export default MainSlide;
