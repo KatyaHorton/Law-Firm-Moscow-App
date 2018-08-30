7cb5ac074bbe90dffce4074b2ffd55badbdae8d4
Katya Horton <ekaterina.horton@gmail.com>
2018-08-30 10:56:25 +0100

    navbar list animation added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 
-
 class NavBar extends Component {
-  render() {
-    return (
-        
-		<div id="nav-bar-container">
-		 	<button id='menu-button'>☰</button>
-			<nav id='nav-bar'></nav>
-		</div>
+	
+	state = {
+		navBarShown: false
+	}
+/*-------------------- functions --------------------*/
 
-    );
+	handleNavBar = () => {
+		if (this.state.navBarShown === true) {
+			this.setState({
+				navBarShown: false
+			})
+		} else {
+			this.setState({
+				navBarShown: true
+			})
+		}
+	}
+	
+	render() {
+		return (
+
+			<div id="nav-bar-container">
+				<button 
+					id='menu-button'
+					onClick = { this.handleNavBar }	
+				>☰</button>
+			
+			{(this.state.navBarShown) && (
+				<ul id='nav-bar'>
+					<li>Главная</li>
+					<li>Услуги</li>
+					<li>Практика</li>
+					<li>Новости</li>
+					<li>Отзывы</li>
+					<li>О нас</li>
+					<li>Контакты</li>
+				</ul>
+			)}
+
+			</div>
+
+		);
   }
 }
 
