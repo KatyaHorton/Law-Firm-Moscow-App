b64f076c5b5046b079f5b5b1df11f3a137a9e4aa
Katya Horton <ekaterina.horton@gmail.com>
2018-09-04 12:02:54 +0100

    Contacts content, including map added

--- src/components/main-slide-content/Contacts.js
+++ src/components/main-slide-content/Contacts.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 class Contacts extends Component {
   render() {
     return (
-        <div className="main-slide">
-			Contacts
+        <div >
+			<map><iframe src="https://yandex.ru/map-widget/v1/-/CBBU6Ec58D" width="90%" height="400" frameborder="1" allowfullscreen="true"></iframe></map>
+			<p>© ООО "ЮРИДИЧЕСКАЯ КОЛЛЕГИЯ"<br/> от 3 марта 2017 г.</p>
+			<p><i className="fas fa-map-marker-alt"></i> город Москва,<br/>станция метро 'Новокузнецкая', <br/>	
+				ул. Раушская набережная дом 4/5 кабинет 42
+			</p>
+			<p><i className="fas fa-envelope"></i> fimida888888@gmail.com</p>
+			<p><i className="fas fa-phone-square"></i> +7-995-502-55-55</p>
         </div>
 
     );
