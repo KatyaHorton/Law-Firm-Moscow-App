bd06dc4c7c5c6eb7f9835336a72b672a01e14760
Katya Horton <ekaterina.horton@gmail.com>
2018-09-06 13:15:45 +0100

    feat: States and handleChange function added to ContactForm

--- src/components/main-slide-content/ContactForm.js
+++ src/components/main-slide-content/ContactForm.js
@@ -3,6 +3,25 @@ import { Form, FormGroup, Input, Label, Button } from 'reactstrap'
 
 
 class ContactForm extends Component {
+  	
+  constructor(){
+	  super();
+	  
+	  this.state = {
+		  name: '',
+		  phone: '', 
+		  message: ''
+	  }
+	  
+// sets the state of the component depending on the change of the input field	  
+	  this.handleChange = this.handleChange.bind(this)	
+  } 
+
+//change state everytime input is entered in the field
+ handleChange = e => {
+	 this.setState({ [e.target.name]: e.target.value }) 
+ }	
+  
 	
   render() {
     return (
