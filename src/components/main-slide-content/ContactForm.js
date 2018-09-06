6a0cb9b70fc489291dc508e3881220a5cc301e28
Katya Horton <ekaterina.horton@gmail.com>
2018-09-06 13:52:22 +0100

    feat: Add handleSubmit function

--- src/components/main-slide-content/ContactForm.js
+++ src/components/main-slide-content/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap'
+import axios from 'axios'
 
 
 class ContactForm extends Component {
@@ -14,7 +15,8 @@ class ContactForm extends Component {
 	  }
 	  
 // sets the state of the component depending on the change of the input field	  
-	  this.handleChange = this.handleChange.bind(this)	
+	  this.handleChange = this.handleChange.bind(this)
+	  this.handleSubmit = this.handleSubmit.bind(this)
   } 
 
 //change state everytime input is entered in the field
@@ -22,10 +24,16 @@ class ContactForm extends Component {
 	 this.setState({ [e.target.name]: e.target.value }) 
  }
  
- handleSubmit(e) {
+ async handleSubmit(e) {
 	 e.preventDefault()
 	 
 	 const { name, email, message } = this.state;
+	 
+	 const form = await axios.post('/api/form', {
+		  name, 
+		  email,
+		  message 
+	 })
  }
 	
   render() {
