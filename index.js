3f5580e92a47d1ea824a09d9ad856f998f39c3ad
Katya Horton <ekaterina.horton@gmail.com>
2018-09-07 10:59:03 +0100

    feat: Set up email to receive info submitted by users

--- index.js
+++ index.js
@@ -7,7 +7,46 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extend: false }))
 
 app.post('/api/form', (req, res) => {
-	console.log(req.body)
+	//console.log(req.body)
+	
+nodemailer.createTestAccount((err, account) => {
+	const htmlEmail = `
+		<h3>Contact Details</h3>
+		<ul>
+			<li>Name: ${ req.body.name }</li>
+			<li>Email: ${ req.body.email }</li>email
+		</ul>
+		<h3>Message</h3>
+		<p>${ req.body.message }</p>
+	`		
+	 
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.ethereal.email',
+		port: 587,
+		auth: {
+			user: 'l7dgnhad7rppr53q@ethereal.email',
+			pass: 'E2cBQPHZWBNHmjwRdB'
+		}
+	})
+	
+	let mailOptions = {
+		from: 'test@testaccount.com',
+		to: 'l7dgnhad7rppr53q@ethereal.email',
+		replyTo: 'test@testaccount.com',
+		subject: 'New Message',
+		text: req.body.message,
+		html: htmlEmail
+	}
+	
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err) {
+			return console.log(err)
+		} 
+		
+		console.log('Message sent: %s', info.message)
+		console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+	})
+}) 
 
 })
 
