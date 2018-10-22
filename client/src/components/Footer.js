8a2eaebadb50316092d076f0e86256c527f4fc1e
Katya Horton <ekaterina.horton@gmail.com>
2018-10-22 11:41:53 +0100

    feat: Add viber/whatsup icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react'
 
 class Footer extends Component {
   render() {
-	  	  
+
     return (
-        
+
 		<footer id="footer">
           <div className="footer-title">
 			<div id='footer-question'>
 				<p>Появились вопросы?</p>
-				<button className="open-form" 
+				<button className="open-form"
 						onClick={this.props.openPopup}>
 						Получить консультацию</button>
-			</div>	
+			</div>
 			<div id='footer-info'>
 			<p id='footer-first-p'>© ООО "ЮРИДИЧЕСКАЯ КОЛЛЕГИЯ"<br/> от 3 марта 2017 г.</p>
-			<p><i className="fas fa-map-marker-alt"></i> город Москва,<br/>станция метро 'Новокузнецкая', <br/>	
+			<p><i className="fas fa-map-marker-alt"></i> город Москва,<br/>станция метро 'Новокузнецкая', <br/>
 				ул. Раушская набережная дом 4/5 кабинет 42
 			</p>
 			<p><a className='footer-link' href='mailto:fimida888888@gmail.com'><i className="fas fa-envelope"></i> fimida888888@gmail.com</a></p>
-			<p><a className='footer-link' href='tel:+7-995-502-55-55'><i className="fas fa-phone-square"></i> +7-995-502-55-55</a></p>
+			<p><a className='footer-link' href='tel:+7-995-502-55-55'><i className="fas fa-phone-square"></i><i class="fab fa-whatsapp-square"></i><i class="fab fa-viber"></i>+7-995-502-55-55</a></p>
 			</div>
 		</div>
 		</footer>
